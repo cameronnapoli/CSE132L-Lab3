d4f434f6cda8d6550d153ceac2bb38069c5c0609
Matt Rommel <rommelmatt@gmail.com>
2017-03-06 15:10:32 -0800

    always @(negedge clk). Writes on falling edge

--- design/regfile.sv
+++ design/regfile.sv
@@ -37,7 +37,7 @@ module regfile(
     // write third port on rising edge of clock
     // register 15 reads PC + 8 instead
 
-    always @(posedge clk)
+    always @(negedge clk)
         if (we3) rf[wa3] <= wd3;
 
     assign rd1 = (ra1 == 4'b1111) ? r15 : rf[ra1];
