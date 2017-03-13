006a71306f13d9af484fc3b735414b672bc9687d
Noah-PC\Noah <njcorrea@uci.edu>
2017-03-13 15:48:29 -0700

    added inputs sources

--- design/hazardunit.sv
+++ design/hazardunit.sv
@@ -1,11 +1,11 @@
 module hazardunit(
+	input logic Match_1E_M, Match_1E_W, Match_2E_M, Match_2E_W,
     output logic StallF,
     output logic StallID,
     output logic FlushID,
     output logic FlushE,
     output logic [1:0] ForwardAE,
     output logic [1:0] ForwardBE,
-    output logic Match, // Input or Output ????!!!!
     output logic RegWriteM,
     output logic RegWriteW,
     output logic MemtoRegE
