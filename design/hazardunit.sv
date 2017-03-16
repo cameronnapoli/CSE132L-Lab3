d8ebaaeac9171da731056720bf5ac3eaa8539a82
Cameron Napoli <cameronnapoli@gmail.com>
2017-03-15 18:47:53 -0700

    Trying to fix merge conflicts

--- design/hazardunit.sv
+++ design/hazardunit.sv
@@ -1,12 +1,11 @@
-module hazardunit(
-	input logic Match_1E_M, Match_1E_W, Match_2E_M, Match_2E_W,
+module hazardunit (
     output logic StallF,
     output logic StallD,
     output logic FlushD,
     output logic FlushE,
     output logic [1:0] ForwardAE,
     output logic [1:0] ForwardBE,
-<<<<<<< HEAD
+
     input logic Match_1E_M,
     input logic Match_1E_W,
     input logic Match_2E_M,
@@ -19,11 +18,6 @@ module hazardunit(
     input logic PCSrcD,
     input logic PCSrcE,
     input logic PCSrcM
-=======
-    output logic RegWriteM,
-    output logic RegWriteW,
-    output logic MemtoRegE
->>>>>>> 7b1bde13f5856048d677d1a34ae7fca1adcee57f
     );
 
 //Match_1E_M = (RA1E == WA3M)
@@ -33,20 +27,20 @@ module hazardunit(
 
 //Match_12D_E = (RA1D == WA3E) + (RA2D == WA3E)
 
-//Forwarding  
-if (Match_1E_M & RegWriteM) 
+//Forwarding
+if (Match_1E_M & RegWriteM)
 	ForwardAE = 10; // SrcAE = ALUOutM
-else if (Match_1E_W & RegWriteW) 
+else if (Match_1E_W & RegWriteW)
 	ForwardAE = 01; // SrcAE = ResultW
-else 
+else
 	ForwardAE = 00; // SrcAE from regfile
 
-if (Match_2E_M & RegWriteM) 
-	ForwardBE = 10; 
-else if (Match_1E_W & RegWriteW) 
-	ForwardBE = 01; 
-else 
-	ForwardBE = 00; 
+if (Match_2E_M & RegWriteM)
+	ForwardBE = 10;
+else if (Match_1E_W & RegWriteW)
+	ForwardBE = 01;
+else
+	ForwardBE = 00;
 
 //RAW
 //IF LDR and WA3E (in execute stage) Matches RA1D or RA2D
