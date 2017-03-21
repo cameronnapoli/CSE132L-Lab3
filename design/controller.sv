c74379e181ce4c01536b9af4334f5a0f162f67ba
Noah-PC\Noah <njcorrea@uci.edu>
2017-03-20 22:43:57 -0700

    Changed some inputs, added comments, still needs work..

--- design/controller.sv
+++ design/controller.sv
@@ -1,6 +1,12 @@
 module controller(input logic clk, reset,
+/*
     input logic [31:12] Instr,
-    input logic [3:0] ALUFlags,
+    input logic [3:0] ALUFlags,  // Flags get passed from IF/ID registers
+								 // to ID/EX and are no longer needed
+*/
+	input logic [27:26] Op,
+	input logic [25:20] Funct,
+	input logic [15:12] Rd,
     output logic [1:0] RegSrc,
     output logic RegWrite,
     output logic [1:0] ImmSrc,
@@ -14,12 +20,15 @@ module controller(input logic clk, reset,
     logic PCS, RegW, MemW;
 
     
-    decoder dec(Instr[27:26], Instr[25:20], Instr[15:12],
+    decoder dec(Op[27:26], Funct[25:20], Rd[15:12],
         FlagW, PCS, RegW, MemW,
         MemtoReg, ALUSrc, ImmSrc, RegSrc, ALUControl,
 	BEDmem, BL);
+	/*
     condlogic cl(clk, reset, Instr[31:28], ALUFlags,
         FlagW, PCS, RegW, MemW,
         PCSrc, RegWrite, MemWrite);
+		*/
+		// Controller gets values from ID/EX registers
 
 endmodule
