0f14aa19e733ce6ebfcaa087751e8ede762eed5f
Matt Rommel <rommelmatt@gmail.com>
2017-03-22 12:16:42 -0700

    BL Regfile

--- design/regfile.sv
+++ design/regfile.sv
@@ -6,7 +6,7 @@
 
 module regfile(
     input logic clk,
-    input logic we3,
+    input logic we3, BL,
     input logic [3:0] ra1, ra2, ra3, wa3,
     input logic [31:0] wd3, r15,
     output logic [31:0] rd1, rd2, rd3);
@@ -20,7 +20,7 @@ module regfile(
 
     always @(negedge clk)
         if (we3) rf[wa3] <= wd3;
-
+        if (BL) r14 <= r15 - 4'b1000;
     assign rd1 = (ra1 == 4'b1111) ? r15 : rf[ra1];
     assign rd2 = (ra2 == 4'b1111) ? r15 : rf[ra2];
     assign rd3 = (ra3 == 4'b1111) ? r15 : rf[ra3];
