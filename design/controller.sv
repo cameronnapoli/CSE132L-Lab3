c8e4f965bad45f53d8b63bb3a28051c4d5cabc37
Matt Rommel <rommelmatt@gmail.com>
2017-03-21 18:49:17 -0700

    Controller Changed

--- design/controller.sv
+++ design/controller.sv
@@ -1,9 +1,5 @@
 module controller(input logic clk, reset,
-/*
-    input logic [31:12] Instr,
-    input logic [3:0] ALUFlags,  // Flags get passed from IF/ID registers
-								 // to ID/EX and are no longer needed
-*/
+
 	input logic [27:26] Op,
 	input logic [25:20] Funct,
 	input logic [15:12] Rd,
@@ -14,8 +10,13 @@ module controller(input logic clk, reset,
     output logic [3:0] ALUControl,
     output logic MemWrite, MemtoReg,
     output logic BEDmem, BL,
-    output logic PCSrc);
-
+    output logic PCSrc,
+// Inputs for CLU /////////
+    input logic [3:0] CondE,
+    input logic [3:0] ALUFlagsE,
+    input logic [1:0] FlagWE,
+    input logic PCSE, RegWE, MemWE,BranchE);
+////////////////////////
     logic [1:0] FlagW;
     logic PCS, RegW, MemW;
 
@@ -24,11 +25,10 @@ module controller(input logic clk, reset,
         FlagW, PCS, RegW, MemW,
         MemtoReg, ALUSrc, ImmSrc, RegSrc, ALUControl,
 	BEDmem, BL);
-	/*
-    condlogic cl(clk, reset, Instr[31:28], ALUFlags,
-        FlagW, PCS, RegW, MemW,
+
+    condlogic cl(clk, reset, CondE, ALUFlagsE,
+        FlagWE, PCSE, RegWE, MemWE, BranchE,
         PCSrc, RegWrite, MemWrite);
-		*/
-		// Controller gets values from ID/EX registers
+
 
 endmodule
