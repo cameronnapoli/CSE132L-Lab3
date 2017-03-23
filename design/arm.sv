68e02d3426662a3328122e5c02f7d61c32d72b8c
Noah-PC\Noah <njcorrea@uci.edu>
2017-03-22 17:44:31 -0700

    Controller I/O..

--- design/arm.sv
+++ design/arm.sv
@@ -6,19 +6,28 @@ module arm(
     output logic [31:0] ALUResultM, WriteDataM, // {datapath} O to dmem
     output logic BEDmem); // {datapath} to dmem
 
-    logic [3:0] ALUFlags, ALUControl;
-    logic RegWrite, ALUSrc, MemtoReg, PCSrc;
-    logic [1:0] RegSrc, ImmSrc;
+    logic [3:0] ALUFlags, ALUControl, CondE, Flags;
+    logic RegWrite, Branch, ALUSrc, MemtoReg, PCSrc, MemWrite, FlagWE, FlagsE;
+    logic [1:0] RegSrc, ImmSrc, FlagWrite;
     logic BL;
     logic [31:0] InstrD; // Intermediary signal to allow Instr to pass through pipe
+	logic PCSE, RegWE, MemWE, BranchE, BLE;
+	logic PCSrcEO, RegWriteEO, MemWriteEO, BranchEO, BLEO;
 
     // MemWrite needs to be intercepted
-    controller c(clk, reset, InstrDController, ALUFlags,
-        RegSrc, RegWrite, ImmSrc,
-        ALUSrc, ALUControl,
-        MemWrite, MemtoReg,
-	    BEDmem, BL,
-	    PCSrc);
+    controller c(clk, reset, InstrD[27:26], InstrD[25:20], InstrD[15:12],
+	// control unit output
+		PCSrc, RegWrite, MemtoReg, MemWrite,
+		ALUControl, Branch, ALUSrc, FlagWrite,ImmSrc,RegSrc,
+		BEDmem, BL,
+		// condlogic input
+		CondE, ALUFlags, FlagWrite, FlagWE, FlagsE,
+		PCSE, RegWE, MemWE, BranchE, BLE,
+		
+		// condlogic output
+		Flags,
+		PCSrcEO, RegWriteEO, MemWriteEO, BranchEO, BLEO
+		);
 
     logic BranchD; // Needs to be added to controller
     // Needs FlagWriteE, CondE, FlagsE, ALUFlags to go to condlogic
